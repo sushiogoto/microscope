0cd33d0f41f0d3e8183bb8bf86142fab361bc378
sushiogoto <yoshio.goto@gmail.com>
2014-12-04 18:34:30 +0800

    Fix pluralize helper.

--- client/helpers/spacebars.js
+++ client/helpers/spacebars.js
@@ -1,7 +1,7 @@
 UI.registerHelper('pluralize', function(n, thing) {
   // fairly stupid pluralizer
   if (n === 1) {
-    return '1' + thing;
+    return '1 ' + thing;
   } else {
     return n + ' ' + thing + 's';
   }
